4bdb82b49aab5e47409b28c941b7eda0d58b730b
JJ Merelo <jjmerelo@gmail.com>
2015-06-12 12:10:38 +0200

    Generado GraphML
    Closes #1
    
    No es demasiado satisfactorio, porque no permite añadir pesos. Va a haber que generarlo directamente en R.

--- traffic-graph.R
+++ traffic-graph.R
@@ -1,9 +1,9 @@
 library(igraph)
 
-traffic.graph <- read.graph("traffic-graph.txt",format="edgelist")
+traffic.graph <- read.graph("traffic-graph.graphml",format="graphml")
 
 plot(traffic.graph, layout = layout.fruchterman.reingold,
-      vertex.size = 25,
+      vertex.size = 10,
       vertex.color="red",
       vertex.frame.color= "white",
       vertex.label.color = "white",
